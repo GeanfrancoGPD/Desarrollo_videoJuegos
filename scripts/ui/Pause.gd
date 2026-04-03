66960db728e362dc5976305091d35ca67ca10923
Mariangel Campos <mariangelcampos0217@gmail.com>
2026-04-03 17:47:04 -0400

    feat: Se agrego la pantalla de pausa al juego.

---
+++ scripts/ui/Pause.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+
+
+@onready var pause_menu: CanvasLayer = $PauseMenu
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		if get_tree().paused:
+			pause_menu.resume_game()
+		else:
+			pause_menu.pause_game()
+
+func _ready():
+	hide()
+
+func resume_game():
+	get_tree().paused = false
+	hide()
+
+func pause_game():
+	get_tree().paused = true
+	show()
+
+func _on_continuar_pressed() -> void:
+	resume_game()
+
+func _on_volver_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/Menu_Principal.tscn")
